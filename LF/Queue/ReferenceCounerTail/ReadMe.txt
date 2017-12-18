c9c9cd43212d9abc70fb224c545b2025e318a587
Roman Peresypkin <roman.peresypkin@listat.com.ua>
2017-12-18 20:27:49 +0200

    [LF][Queue] Reference counting tail

---
+++ LF/Queue/ReferenceCounerTail/ReadMe.txt
@@ -0,0 +1,40 @@
+========================================================================
+    CONSOLE APPLICATION : ReferenceCounerTail Project Overview
+========================================================================
+
+AppWizard has created this ReferenceCounerTail application for you.
+
+This file contains a summary of what you will find in each of the files that
+make up your ReferenceCounerTail application.
+
+
+ReferenceCounerTail.vcxproj
+    This is the main project file for VC++ projects generated using an Application Wizard.
+    It contains information about the version of Visual C++ that generated the file, and
+    information about the platforms, configurations, and project features selected with the
+    Application Wizard.
+
+ReferenceCounerTail.vcxproj.filters
+    This is the filters file for VC++ projects generated using an Application Wizard. 
+    It contains information about the association between the files in your project 
+    and the filters. This association is used in the IDE to show grouping of files with
+    similar extensions under a specific node (for e.g. ".cpp" files are associated with the
+    "Source Files" filter).
+
+ReferenceCounerTail.cpp
+    This is the main application source file.
+
+/////////////////////////////////////////////////////////////////////////////
+Other standard files:
+
+StdAfx.h, StdAfx.cpp
+    These files are used to build a precompiled header (PCH) file
+    named ReferenceCounerTail.pch and a precompiled types file named StdAfx.obj.
+
+/////////////////////////////////////////////////////////////////////////////
+Other notes:
+
+AppWizard uses "TODO:" comments to indicate parts of the source code you
+should add to or customize.
+
+/////////////////////////////////////////////////////////////////////////////
---
+++ LF/Queue/ReferenceCounerTail/ReferenceCounerTail.cpp
@@ -0,0 +1,143 @@
+#include "stdafx.h"
+
+#include <memory>
+#include <atomic>
+
+using namespace std;
+
+template <typename T>
+class LFQueueRCTail
+{
+private:
+	struct Node;
+
+	struct CountedNodePtr {
+		int externalCount;
+		Node* ptr;
+	};
+
+	atomic<CountedNodePtr> head;
+	atomic<CountedNodePtr> tail;
+
+	struct NodeCounter {
+		unsigned internalCount:30;
+		unsigned externalCounters:2;
+	};
+
+	struct Node
+	{
+		atomic<T*> data;
+		atomic<NodeCounter> count;
+		CountedNodePtr next;
+
+		Node()
+		{
+			NodeCounter newCount;
+			newCount.internalCount = 0;
+			newCount.externalCounters = 2;
+			count.store(newCount);
+
+			next.ptr = nullptr;
+			next.externalCount = 0;
+		}
+
+		void ReleaseRef()
+		{
+			NodeCounter oldCounter = count.load();
+			NodeCounter newCounter;
+			do
+			{
+				newCounter = oldCounter;
+				--newCounter.internalCount;
+			} while (!count.compare_exchange_strong(oldCounter, newCounter))
+				;
+			if(!newCounter.internalCount && !newCounter.externalCounters)
+				delete this;
+		}
+	};
+
+	static void IncreaseExternalCounter(atomic<CountedNodePtr>& counter, CountedNodePtr& old_counter);
+	static void FreeExternalCounter(CountedNodePtr& oldNodePtr);
+public:
+	void push(T newValue);
+	unique_ptr<T> pop();
+};
+
+template <typename T>
+void LFQueueRCTail<T>::IncreaseExternalCounter(atomic<CountedNodePtr>& counter, CountedNodePtr& old_counter)
+{
+	CountedNodePtr new_counter;
+	do
+	{
+		new_counter = old_counter;
+		++new_counter.externalCount;
+	}
+	while(!counter.compare_exchange_strong(old_counter, new_counter));
+	old_counter.externalCount = new_counter.externalCount;
+}
+
+template <typename T>
+void LFQueueRCTail<T>::FreeExternalCounter(CountedNodePtr& oldNodePtr)
+{
+	Node* const ptr = oldNodePtr.ptr;
+	int const countIncrease = oldNodePtr.externalCount - 2;
+	NodeCounter oldCounter = ptr->count.load();
+	NodeCounter newCounter;
+	do
+	{
+		newCounter = oldCounter;
+		--newCounter.externalCounters;
+		newCounter.internalCount += countIncrease;
+	} while (!ptr->count.compare_exchange_strong(oldCounter, newCounter))
+		;
+	if(!newCounter.internalCount && !newCounter.externalCounters)
+		delete ptr;
+}
+
+template <typename T>
+void LFQueueRCTail<T>::push(T newValue)
+{
+	unique_ptr<T> newData(new T(newValue));
+	CountedNodePtr newNext;
+	newNext.ptr = new Node;
+	newNext.externalCount = 1;
+	CountedNodePtr oldTail = tail.load();
+	for(;;) {
+		IncreaseExternalCounter(tail, oldTail);
+		T* oldData = nullptr;
+		if(oldTail.ptr->data.compare_exchange_strong(oldData, newData.get()))
+		{
+			oldTail.ptr->next = newNext;
+			oldTail = tail.exchange(newNext);
+			FreeExternalCounter(oldTail);
+			newData.release();
+			break;
+		}
+		oldTail.ptr->ReleaseRef();
+	}
+}
+
+template <typename T>
+std::unique_ptr<T> LFQueueRCTail<T>::pop()
+{
+	CountedNodePtr oldHead = head.load();
+	for (;;) {
+		IncreaseExternalCounter(head, oldHead);
+		Node* const ptr = oldHead.ptr;
+		if(ptr == tail.load().ptr) {
+			ptr->ReleaseRef();
+			return unique_ptr<T>();
+		}
+		if(head.compare_exchange_strong(oldHead, ptr->next)) {
+			T* const res = ptr->data.exchange(nullptr);
+			FreeExternalCounter(oldHead);
+			return unique_ptr<T>(res);
+		}
+		ptr->ReleaseRef();
+	}
+}
+
+int main()
+{
+	return 0;
+}
