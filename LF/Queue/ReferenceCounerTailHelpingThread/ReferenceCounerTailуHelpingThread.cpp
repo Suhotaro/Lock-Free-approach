2330e27a1eae33b04d6a2ca3cbf19508ae791d49
Ropero <suhotaro@gmail.com>
2018-02-07 02:10:45 +0200

    [LF] Queue with helping thread

--- LF/Queue/ReferenceCounerTailHelpingThread/ReferenceCounerTailуHelpingThread.cpp
+++ LF/Queue/ReferenceCounerTailHelpingThread/ReferenceCounerTailуHelpingThread.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 template <typename T>
-class LFQueueRCTail
+class LFQueueRCTailHelpingThread
 {
 private:
 	struct Node;
@@ -20,15 +20,15 @@ private:
 	atomic<CountedNodePtr> tail;
 
 	struct NodeCounter {
-		unsigned internalCount:30;
-		unsigned externalCounters:2;
+		unsigned internalCount : 30;
+		unsigned externalCounters : 2;
 	};
 
 	struct Node
 	{
 		atomic<T*> data;
 		atomic<NodeCounter> count;
-		CountedNodePtr next;
+		atomic<CountedNodePtr> next;
 
 		Node()
 		{
@@ -36,9 +36,6 @@ private:
 			newCount.internalCount = 0;
 			newCount.externalCounters = 2;
 			count.store(newCount);
-
-			next.ptr = nullptr;
-			next.externalCount = 0;
 		}
 
 		void ReleaseRef()
@@ -51,34 +48,45 @@ private:
 				--newCounter.internalCount;
 			} while (!count.compare_exchange_strong(oldCounter, newCounter))
 				;
-			if(!newCounter.internalCount && !newCounter.externalCounters)
+			if (!newCounter.internalCount && !newCounter.externalCounters)
 				delete this;
 		}
 	};
 
 	static void IncreaseExternalCounter(atomic<CountedNodePtr>& counter, CountedNodePtr& old_counter);
 	static void FreeExternalCounter(CountedNodePtr& oldNodePtr);
+
+	void SetNewTail(CountedNodePtr &oldTail, CountedNodePtr const &newTail)
+	{
+		Node * const currentTailPtr = oldTail.ptr;
+		while (!tail.compare_exchange_weak(oldTail, newTail) && oldTail.ptr == currentTailPtr)
+			;
+		if (oldTail.ptr == currentTailPtr)
+			FreeExternalCounter(oldTail);
+		else
+			currentTailPtr->ReleaseRef();
+	}
+
 public:
 	void push(T newValue);
 	unique_ptr<T> pop();
 };
 
 template <typename T>
-void LFQueueRCTail<T>::IncreaseExternalCounter(atomic<CountedNodePtr>& counter, CountedNodePtr& old_counter)
+void LFQueueRCTailHelpingThread<T>::IncreaseExternalCounter(atomic<CountedNodePtr>& counter, CountedNodePtr& old_counter)
 {
 	CountedNodePtr new_counter;
 	do
 	{
 		new_counter = old_counter;
 		++new_counter.externalCount;
-	}
-	while(!counter.compare_exchange_strong(old_counter, new_counter))
+	} while (!counter.compare_exchange_strong(old_counter, new_counter))
 		;
 	old_counter.externalCount = new_counter.externalCount;
 }
 
 template <typename T>
-void LFQueueRCTail<T>::FreeExternalCounter(CountedNodePtr& oldNodePtr)
+void LFQueueRCTailHelpingThread<T>::FreeExternalCounter(CountedNodePtr& oldNodePtr)
 {
 	Node* const ptr = oldNodePtr.ptr;
 	int const countIncrease = oldNodePtr.externalCount - 2;
@@ -91,45 +99,61 @@ void LFQueueRCTail<T>::FreeExternalCounter(CountedNodePtr& oldNodePtr)
 		newCounter.internalCount += countIncrease;
 	} while (!ptr->count.compare_exchange_strong(oldCounter, newCounter))
 		;
-	if(!newCounter.internalCount && !newCounter.externalCounters)
+	if (!newCounter.internalCount && !newCounter.externalCounters)
 		delete ptr;
 }
 
 template <typename T>
-void LFQueueRCTail<T>::push(T newValue)
+void LFQueueRCTailHelpingThread<T>::push(T newValue)
 {
 	unique_ptr<T> newData(new T(newValue));
 	CountedNodePtr newNext;
 	newNext.ptr = new Node;
 	newNext.externalCount = 1;
 	CountedNodePtr oldTail = tail.load();
-	for(;;) {
+
+	for (;;) {
 		IncreaseExternalCounter(tail, oldTail);
 		T* oldData = nullptr;
-		if(oldTail.ptr->data.compare_exchange_strong(oldData, newData.get()))
+
+		if (oldTail.ptr->data.compare_exchange_strong(oldData, newData.get()))
 		{
-			oldTail.ptr->next = newNext;
-			oldTail = tail.exchange(newNext);
-			FreeExternalCounter(oldTail);
+			CountedNodePtr oldNext = { 0 };
+			if (!oldTail.ptr->next.compare_exchange_strong(oldNext, newNext))
+			{
+				delete newNext.ptr;
+				newNext = oldNext;
+			}
+			SetNewTail(oldTail, newNext);
 			newData.release();
 			break;
 		}
-		oldTail.ptr->ReleaseRef();
+		else
+		{
+			CountedNodePtr oldNext = { 0 };
+			if (oldTail.ptr->next.compare_exchange_strong(oldNext, newNext))
+			{
+				oldNext = newNext;
+				newNext.ptr = new Node;
+			}
+			SetNewTail(oldTail, oldNext);
+		}
 	}
 }
 
 template <typename T>
-std::unique_ptr<T> LFQueueRCTail<T>::pop()
+std::unique_ptr<T> LFQueueRCTailHelpingThread<T>::pop()
 {
 	CountedNodePtr oldHead = head.load();
 	for (;;) {
 		IncreaseExternalCounter(head, oldHead);
 		Node* const ptr = oldHead.ptr;
-		if(ptr == tail.load().ptr) {
-			ptr->ReleaseRef();
+		if (ptr == tail.load().ptr) {
 			return unique_ptr<T>();
 		}
-		if(head.compare_exchange_strong(oldHead, ptr->next)) {
+
+		CountedNodePtr next = ptr->next.load();
+		if (head.compare_exchange_strong(oldHead, next)) {
 			T* const res = ptr->data.exchange(nullptr);
 			FreeExternalCounter(oldHead);
 			return unique_ptr<T>(res);
@@ -140,5 +164,11 @@ std::unique_ptr<T> LFQueueRCTail<T>::pop()
 
 int main()
 {
+	LFQueueRCTailHelpingThread<int> v;
+
+	v.push(10);
+
+	auto t = v.pop();
+
 	return 0;
 }
